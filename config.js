da670af132a9f30d3471d97c534befb62147ff42
myeongeun <myeongeunyang495@gmail.com>
2026-09-12 17:20:49 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // GitHub Pages 사용 시 Kakao Developers > 플랫폼 키 > JavaScript 키 > JavaScript SDK 도메인에
 // https://YOUR_USERNAME.github.io 를 등록하세요.
 window.APP_CONFIG = {
-  KAKAO_JS_KEY: ""
+  KAKAO_JS_KEY: "fc8fd45a1e1c2a553dc2393ccaa27f64"
 };
