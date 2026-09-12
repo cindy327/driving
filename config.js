bf64b0fd0dc883953486b079ccdff3a3f093817c
myeongeun <myeongeunyang495@gmail.com>
2026-09-12 17:11:22 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Kakao Developers에서 발급받은 "JavaScript 키"를 아래에 입력하세요.
+// GitHub Pages 사용 시 Kakao Developers > 플랫폼 키 > JavaScript 키 > JavaScript SDK 도메인에
+// https://YOUR_USERNAME.github.io 를 등록하세요.
+window.APP_CONFIG = {
+  KAKAO_JS_KEY: ""
+};
